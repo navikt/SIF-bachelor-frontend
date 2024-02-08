b30f3075451dcdf4adb90f1998912bacc9cfef67
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2024-02-08 17:33:34 +0100

    separated navbar and footer from the landingpage so that they are separate components

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,15 @@ import Greeting from './components/Greeting';
 import Journalpost from './components/Journalpost';
 import Get from './components/Get'
 import LandingPage from './pages/landing/LandingPage';
+import Footer from './components/common/footer/Footer';
+import Navbar from './components/common/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <LandingPage></LandingPage>
-
+      <Footer></Footer>
       {/*       
       <Greeting/>
       <Get/>
