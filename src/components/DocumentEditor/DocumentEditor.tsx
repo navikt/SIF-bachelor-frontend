f0b5f3cbe7dada09203ce5866b45202b0a23f4a1
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2024-04-02 19:53:47 +0200

    Gotten started on the data transfer. Next up maybe add in a boolean to documentViewer so that if we are in the documentEditor component, it won't update the documents in the PDF reader if we select some specific documents in teh documentEditor

--- src/components/DocumentEditor/DocumentEditor.tsx
+++ src/components/DocumentEditor/DocumentEditor.tsx
@@ -1,5 +1,5 @@
-import {useRef } from "react"
-import {Button, Modal, TextField } from "@navikt/ds-react"
+import {useRef, useState } from "react"
+import {Button, Modal, TextField, Select } from "@navikt/ds-react"
 import { PencilIcon } from "@navikt/aksel-icons";
 import { IDocument } from "../types";
 import {DocumentViewer} from "../DocumentViewer/DocumentViewer";
@@ -15,8 +15,46 @@ export const DocumentEditor = ({ journalpostId, tittel, journalposttype, datoOpp
     addDocument: (document: IDocument) => void,
     documents: IDocument[],
 }) => {
+    
+    // Manage state for the input fields
+    const [brukerId, setBrukerId] = useState('');
+    const [tittelen, setTittel] = useState(tittel);
+    const [journalposttypen, setType] = useState(journalposttype);
+    const [statusen, setStatus] = useState(journalstatus);
+    const [temaet, setTema] = useState(tema);
+
+    // Error message
+    const [errorMessage, setErrorMessage] = useState('');
+
     const ref = useRef<HTMLDialogElement>(null);
 
+    const formatDate = (date: Date) => {
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear().toString();
+        return `${day}.${month}.${year}`
+    }  ;  
+
+    const splitDocs = () => {
+        console.log("testing" + tittelen + journalposttypen + statusen + temaet);
+
+        const token = sessionStorage.getItem("token");
+
+        if(!token) {
+            setErrorMessage("Du må logge inn for å søke!");
+            return;
+        }
+        const currentDate = formatDate(new Date());
+        // Opprett JSON body med userId
+        const requestBody = {
+            brukerId: {
+              id: brukerId,
+              type: "FNR" 
+            },
+            
+          };
+
+    }
     return(
         <div>
             <Button 
@@ -27,7 +65,7 @@ export const DocumentEditor = ({ journalpostId, tittel, journalposttype, datoOpp
 
             <Modal ref={ref} header={{ heading: "Splitt Opp Dokumenter" }} width={600}>
                 <Modal.Body>
-                    <form method="dialog" id="skjema" onSubmit={() => alert("onSubmit")}>
+                    <form method="dialog" id="skjema" onSubmit={splitDocs}>
                         <TextField      
                             label="ID"      
                             value={journalpostId}
@@ -36,28 +74,30 @@ export const DocumentEditor = ({ journalpostId, tittel, journalposttype, datoOpp
                         />
                         <TextField      
                             label="Tittel"      
-                            value={tittel}
+                            value={tittelen}
+                            onChange={(event) => setTittel(event.target.value)}
                             className="inputBox"
                         />
-                        <TextField      
-                            label="Type"      
-                            value={journalposttype}
-                            className="inputBox"
-                        />
+                        <Select label="Type">
+                            <option value="I">Inngående</option>
+                            <option value="U">Utgående</option>
+                            <option value="N">Notat</option>
+                        </Select>
                         <TextField      
                             label="Dato"      
                             value={datoOpprettet}
                             className="inputBox"
                             readOnly
                         />
-                        <TextField      
-                            label="Status"      
-                            value={journalstatus}
-                            className="inputBox"
-                        />
+                        <Select label="Status">
+                            <option value="JOURNALFOERT">Journalført</option>
+                            <option value="FERDIGSTILT">Utgående</option>
+                            <option value="NOTAT">Notat</option>
+                        </Select>
                         <TextField      
                             label="Tema"      
-                            value={tema}
+                            value={temaet}
+                            onChange={(event) => setTema(event.target.value)}
                             className="inputBox"
                         />
                         <h2>Velg dokumenter</h2>
