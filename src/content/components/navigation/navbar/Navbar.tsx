fd2575285875f540bb1698718e5196db91811d06
GisleGaren <89572928+GisleGaren@users.noreply.github.com>
2024-05-10 17:55:33 +0200

    Automatically removes token in session storage and token expiration and automatically puts log out status once expired

--- src/content/components/navigation/navbar/Navbar.tsx
+++ src/content/components/navigation/navbar/Navbar.tsx
@@ -14,6 +14,26 @@ const Navbar = () => {
   
   // We have a useState hook to check if there is a token stored in sessionStorage and we set the isLoggedIn to true if found.
   // Another boolean hook which sets itself to true if a token stored in sessionStorage is found and we set the button's content to "Logg ut" if true.
+  const [buttonText, setButtonText] = useState(sessionStorage.getItem('token') ? "Logg ut" : "Logg inn");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Checking token validity...");
+      if (!isTokenValid()) {
+        // Handle token expiration
+        setErrorMessage({message: "Din sesjon har utløpt. Vennligst logg inn igjen.", variant: "info"})
+        sessionStorage.removeItem('token'); // Remove the token from sessionStorage
+        sessionStorage.removeItem('token_expiration');
+        setIsLoggedIn(false);
+        setErrorMessage({message: "Logget ut!", variant:"info"})
+        setButtonText("Logg inn");
+       // toggleLogin(); // Comment out or remove this if we don't want to automatically log in again
+      }
+    }, 60000); // Check every minute
+  
+    // Need the cleanup function when the component unmounts or before it re-renders to prevent memory leak from older unused intervals
+    return () => clearInterval(interval);
+  }, []);
 
   const returnHome = () =>{
     navigate("/")
